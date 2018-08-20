8b0f07de7ecaf24a583b5147ecdfb1b7f3e8de18
Tormozila <tormozila@gmail.com>
2018-08-20 16:54:01 +0300

    +visit +user record

--- app.rb
+++ app.rb
@@ -12,8 +12,24 @@ get '/start' do
   erb '<h1> Start </h1>'
 end
 
-get '/start2' do
-  erb '<h1> Start2 </h1>'
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+
+  @name_ = params[:name_]
+  @cell_number = params[:cell_]
+  @date_time = params[:date_]
+
+  @title = 'Thank you'
+  @message = "#{@user_id} we will be waiting for you at #{@date_time}. Your cell #{@cell_number}"
+
+  f = File.open './public/users.txt', 'a'
+  f.write "user: #{@user_id}; cell: #{@cell_number}; date: #{@date_time}"
+  f.close
+  erb :visit
+
 end
 
 
